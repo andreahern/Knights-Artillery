452ffed552ba2103427b50f51510fec690f59841
Abraham Hernandez <abeherham@knights.ucf.edu>
2020-10-10 00:19:45 -0400

    Initialized Client Frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="text-4xl font-bold">
+          Knight's Artillery
+        </h1>
+    </div>
+  );
+}
+
+export default App;
