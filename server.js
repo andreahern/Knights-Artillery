d4892338f10a4e6e550094cc20f5a640eba55760
Abraham Hernandez <abeherham@knights.ucf.edu>
2020-10-09 22:53:27 -0400

    Began implementation of sockets

--- server.js
+++ server.js
@@ -6,10 +6,34 @@ const connectDB = require('./config/db');
 
 connectDB();
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/sessions', require('./routes/sessions'))
 
+io.on('connection', (socket) => {
+    socket.on('joinSession', ({id, name}) => {
+        socket.join(id);
+        if (io.sockets.adapter.rooms[id].length < 2) {
+            io.sockets.adapter.rooms[id].host = name;
+            io.sockets.in(id).emit("showConnections", {
+              activePlayers: io.sockets.adapter.rooms[id].length,
+              host: name,
+            });
+          } 
+        else {
+            io.sockets.in(id).emit("showConnections", {
+                activePlayers: io.sockets.adapter.rooms[id].length,
+                host: io.sockets.adapter.rooms[id].host,
+                guest: name
+            });
+        }
+    });
+    socket.on("disconnect", () => socket.removeAllListeners());
+})
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
