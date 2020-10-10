9233c4f0c21e8575aa43ce7860d870ece4a610d4
Abraham Hernandez <abeherham@knights.ucf.edu>
2020-10-09 22:07:57 -0400

    Added Session Creation

---
+++ models/session.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const sessionSchema = new mongoose.Schema({
+    joinCode: {
+        type: String,
+        required: true,
+        unique: true   
+    },
+    hostID: {
+        type: String,
+        required: true
+    },
+    guestID: {
+        type: String,
+    },
+    hostName: {
+        type: String,
+        required: true
+    },
+    guestName: {
+        type: String,
+    }
+});
+
+const Session = mongoose.model('Session', sessionSchema);
+exports.Session = Session;
--- routes/sessions.js
+++ routes/sessions.js
@@ -1,8 +1,27 @@
+const {Session} = require('../models/session');
 const express = require('express');
 const router = express.Router();
+const uniqid = require('uniqid');
 
 router.get('/', async(req, res) => {
-    res.send("List of Sessions");
+    try {
+        const sessions = await Session.find();
+        res.send(sessions);
+    } catch(err) {
+        res.error(err);
+    }
+});
+
+router.post('/', async(req, res) => {
+    try {
+        const {hostID, hostName} = req.body;
+        const joinCode = uniqid();
+        const session = new Session({joinCode, hostID, hostName});
+        session.save();
+        res.send(session);
+    } catch(err) {
+        res.error(err);
+    }
 });
 
 module.exports = router;
