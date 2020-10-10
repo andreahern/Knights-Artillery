b6ff0bfc5305c403c2a44fad69b401f0f553ffbb
Abraham Hernandez <abeherham@knights.ucf.edu>
2020-10-09 20:56:52 -0400

    Added connection to DB / Began working on routes

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config({path: './config/config.env'});
+const connectDB = require('./config/db');
 
-
+connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+app.use('/sessions', require('./routes/sessions'))
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
